18c8f1c31457c4feda6e81231f426ea5b0ba0cf2
Dharani <dharaniramisetty12345@gmail.com>
2024-06-13 16:17:02 +0530

    This is java file

---
+++ arith1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class arith
+{
+	public static void main (String arg[])
+}
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int b=in.nextInt();
+	System.out.println("add:"+(a+b));
+	System.out.println("sub:"+(a-b));
+	System.out.println("product:"+(a*b));
+	System.out.println("remainder:"+(a%b));
+	System.out.println("division:"+(float)a/b);
+	}
+	}
+
